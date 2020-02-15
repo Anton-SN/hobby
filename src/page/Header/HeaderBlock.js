cf4e91b04c506aef5ecf418649ce11d2349e8103
Anton Sivagin <ansn.onward@gmail.com>
2020-02-15 16:01:16 +0300

    Изменил хеадер

--- src/page/Header/HeaderBlock.js
+++ src/page/Header/HeaderBlock.js
@@ -1,24 +1,23 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import styles from "./HeaderBlock.module.css";
 
 export default class Header extends Component {
   render() {
+    const linksDate = [ 
+      { path: "/hacks", label: "Фичи", id:"1", }, 
+      { path: "/test", label: "Тестовые задания", id:"2", }, 
+      { path: "/about", label: "Обо мне", id:"3", }, 
+      { path: "/apps", label: "Приложения", id:"4", }, ];
+      const linkElem = linksDate.map(link => 
+        <li key={link.id} className = {`${styles.header__link} ${styles.link}`}>
+          <Link to={link.path}>{link.label}</Link>
+        </li>)
     return (
-      <div className = "header">
+      <div className = {styles.header}>
         <h1>Ссылки Хеадер</h1>
-          <ul className = "header__links links">
-            <li>
-              <Link to="/hacks">Фичи</Link>
-            </li>
-            <li>
-              <Link to="/test">Тестовые задания</Link>
-            </li>
-            <li>
-              <Link to="/about">Обо мне</Link>
-            </li>
-            <li>
-              <Link to="/apps">Приложения</Link>
-            </li>
+          <ul className = {`${styles.header__list} ${styles.list}`}>
+            {linkElem || "Пусто"}
           </ul>
       </div>
     )
